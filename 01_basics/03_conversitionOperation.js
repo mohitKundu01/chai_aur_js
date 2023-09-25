331e432d949d68843e703f3a978be01030e21fb1
mohitKundu01 <144689971+mohitKundu01@users.noreply.github.com>
2023-09-25 05:41:48 +0000

    Operators

---
+++ 01_basics/03_conversitionOperation.js
@@ -0,0 +1,63 @@
+let score = "33";
+
+// console.log(typeof score)
+
+let valueInInt = Number(score);
+// console.log(valueInInt);
+// console.log(typeof valueInInt)
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1
+// false -> 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 -> true
+// 0 -> false
+// "" -> false
+// "hitesh" -> true
+
+let someNumber = 22;
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ******************** Operations ***********************
+
+let value = 20;
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(3/3)
+// console.log(3%2)
+
+
+let str1 = "Hello"
+let str2 = " Kundu"
+// console.log(str1+str2)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log((3+4) * 5 % 3)
+
+// console.log(+true) // convert into integer
+// console.log(+"") // convert into integer value is 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
