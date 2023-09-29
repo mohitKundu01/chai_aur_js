6bd7650b900d7295ca6359f28012fdc01dcf8ebc
mohitKundu01 <144689971+mohitKundu01@users.noreply.github.com>
2023-09-29 11:40:51 +0000

    Math and numbers

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,39 @@
+const score = 400;
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ++++++++++++++++ Maths  ++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.9))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(3, -9, 10, 3))
+// console.log(Math.max(22, 1, 55, 8))
+
+
+console.log(Math.random())   // That's is greater than or equal to 0 and less than 1.
+
+let value = Math.random();
+console.log(value);
+console.log(value*10);
+console.log(Math.floor(value*10) + 1)
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(value*(max-min+1)+min));
+
+
