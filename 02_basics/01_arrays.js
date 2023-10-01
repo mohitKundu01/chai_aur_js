9fdb34d19d39ea4d15f623427a5a978380fa64c7
mohitKundu01 <144689971+mohitKundu01@users.noreply.github.com>
2023-10-01 12:22:06 +0000

    Array Part1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+// Array
+// Shallow Copy - of an object is a copy whose properties share the same reference.
+// Deep Copy - of an object is a copy whose properties do not share the same reference.
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeroes = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+console.log(myArr[1])
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+myArr.unshift(9)
+myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(9))
+
+const newArr = myArr.join()
+
+console.log(myArr)
+console.log(newArr)
+console.log(typeof newArr)
+
+// slice, splice
+
+console.log("A ", myArr)
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1)
+console.log("B ", myArr)
+ 
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr)
+console.log(myn2)
